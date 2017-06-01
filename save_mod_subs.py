fd349dc30f9176dca824f6716781bed2e5987d34
Ella Guest <ellamguest@gmail.com>
2017-06-01 15:58:51 +0100

    updating run.sh

--- save_mod_subs.py
+++ save_mod_subs.py
@@ -7,29 +7,10 @@ Created on Thu Mar  2 14:23:59 2017
 """
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
 import json
 import time
 from datetime import date
-import pickle
 import os
-import logging
-import logging.handlers
-
-LOGGING_DIR = 'logging/logs.txt'
-
-
-### GETTING REDDIT API CREDENTIALS
-with open('credentials.json', 'r') as f:
-     data = json.load(f)
-
-headers={'User-agent':data['user_agent']}
-
-### CREATE EDGELIST + NODELIST FUNCTIONS
-def make_soup(url):
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
-    return soup
 
 def get_mod_type(df, name, mode=0):
     current = df['pubdate'].max()
@@ -49,121 +30,95 @@ def get_mod_type(df, name, mode=0):
     else:
         return 'ERROR'
 
-def get_lists(df):
-    ''' return edgelist, nodelist from df with
-        colums (moderator) 'name' and 'sub'
-    '''
-    names = list(set((df[df['name']!='AutoModerator']['name'])))
+def mod_list(name, sub):
+    url = 'https://www.reddit.com/user/{}/moderated_subreddits.json'.format(name)
+    r = requests.get(url, {'user-agent':'ThisIsABot'})
+    r = requests.get('https://www.reddit.com/user/{}/moderated_subreddits.json'.format(name), headers={'user-agent': 'ThisIsABot'})
+    data = r.json()
 
-    print('Pulling moderated sub lists...')
-    logging.info('Pulling moderated sub lists...')
+    path = os.path.join('moding-data', '{}'.format(sub), str(date.today()), '{}.json'.format(name))
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    
+    with open(path, 'w') as f:
+        json.dump(data, f)
+
+#    path = os.path.join('moding-data', '{}'.format(sub), str(date.today()), '{}'.format(name))
+#    os.makedirs(os.path.dirname(path), exist_ok=True)
+#    
+#    with open(path, 'r') as f:
+#        try:
+#            data = json.load(f)
+#        except:
+#            data = {}
+    
     d = {}
-    errors = [] # errors are mods for whom mod sub lists were not retrieved
+    if 'data' in data:
+        for item in data['data']:
+            d[item['sr']] = item['subscribers']
+    elif 'message' in data:
+        print(data['message'])
+        d[data['message']] = data['message']
+    else:
+        d[0] = 0
+    
+    df = pd.DataFrame.from_dict(d, orient='index')
+    df.reset_index(inplace=True)
+    df.rename(columns={0 : 'subscribers', 'index':'sub'}, inplace=True)
+    df['mod'] = name
+    df['date'] = date.today()
+    
+    time.sleep(2)
+      
+    return df
+ 
+def edgelist(sub, df):
+    names = df['name'].unique()
+    
+    dfs = []
     n = len(names)
     for name in names:
         print(n, name)
+        dfs.append(mod_list(name, sub))
         n -= 1
-        url = 'https://www.reddit.com/user/{}'.format(name)
-        soup = make_soup(url)
-        try:
-            table = soup.find('ul', {'id':'side-mod-list'})
-            items = table.find_all('li')
-            subs = []
-            for item in items:
-                subs.append(item.a['title'])
-            d[name] = subs
-        except:
-            errors.append(name)
-            print('ERROR')
-            pass
-        time.sleep(1)
-    print('moderated subreddit list not captured for', len(errors), 'moderators')
-          
-    edgelist = [] 
-    for key in d.keys():
-        for value in d[key]:
-            edgelist.append([key, value])
     
-    edgelist = pd.DataFrame(edgelist)
-    edgelist.columns = ['mod', 'sub']
+    edgelist = pd.concat(dfs)
+    edgelist.reset_index(drop=True, inplace=True)
     
+    edgelist_path = os.path.join('moding-data', '{}'.format(sub), str(date.today()), 'lists', 'edgelist.csv')
+    os.makedirs(os.path.dirname(edgelist_path), exist_ok=True)
+    edgelist.to_csv(edgelist_path, index=False)
+
+    
+    return edgelist
+
+
+def nodelist(sub, edgelist, df):
     nodelist = pd.DataFrame(list(set(edgelist['mod'])) + list(set(edgelist['sub'])))
     modes = [1]*len(list(set(edgelist['mod']))) + [0]*len(list(set(edgelist['sub'])))
     nodelist['type'] = modes
     nodelist.rename(columns={0 : 'mod'}, inplace=True)
     nodelist['mod_types'] = nodelist.apply(lambda row: get_mod_type(df, row['mod'], row['type']), axis=1)
     
-    mod_types = []
-    
-    current = df['pubdate'].max()
-    subset = df[df['name']==name]
-    for name in edgelist['mod']:
-        print(name)
-        mode = nodelist[nodelist['mod']==name]
-        if mode == 0:
-            print(0)
-        if current not in list(subset['pubdate']):
-            if '+all' not in list(subset['permissions']):
-                print(1)
-            else:
-                print(2)
-        if current in list(subset['pubdate']):
-            if '+all' not in list(subset['permissions']):
-                print(3)
-            else:
-                print(4)
-        else:
-            print('ERROR')
-        
-
-def save_lists(sub):
-    logging.info('Running for {}...'.format(sub))
-    '''sub = 'td' or 'cmv'''
-    df_path = os.path.join('data', '{}'.format(sub), 'mod-hist')
-    os.makedirs(os.path.dirname(df_path), exist_ok=True)
-    
-    logging.info('opening mod hist')
-    df = pd.read_csv(df_path, index_col=0)
-    logging.info('Getting lists...')
-    df = df.head(n=20)
-    errors, edgelist, nodelist = get_lists(df)
-    
-    logging.info('storing errors')
-    error_path = os.path.join('data', sub, 'errors', str(date.today()))
-    os.makedirs(os.path.dirname(error_path), exist_ok=True)
-    with open(error_path, 'wb') as fp:
-        pickle.dump(errors,fp)
-    
-    logging.info('storing edgelist ')
-    edgelist_path = os.path.join('data', sub, 'edgelists', str(date.today()))
-    os.makedirs(os.path.dirname(edgelist_path), exist_ok=True)
-    edgelist.to_csv(edgelist_path, index=False)
-
-    logging.info('storing nodelist')
-    nodelist_path = os.path.join('data', sub, 'nodelists', str(date.today()))
+    nodelist_path = os.path.join('moding-data', '{}'.format(sub), str(date.today()), 'lists', 'nodelist.csv')
     os.makedirs(os.path.dirname(nodelist_path), exist_ok=True)
     nodelist.to_csv(nodelist_path, index=False)
+
+    return nodelist
+
+def run(sub):
+    #update modtimelines first
+    # sub = 'cmv' or 'td
+    df_path = os.path.join('tidy-data', '{}-mod-hist.csv'.format(sub))
+    df = pd.read_csv(df_path, index_col=0)
+    os.makedirs(os.path.dirname(df_path), exist_ok=True)
+    print('MAKING {} EDGELIST...'.format(sub))
+    e = edgelist(sub, df)
+    print('MAKING {} NODELIST...'.format(sub))
+    nodelist(sub, e, df)
     
-    logging.info('Finished for {}'.format(sub))
+def run_both():
+    run('td')
+    print('')
+    print('')
+    run('cmv')
 
-
-
-### UPDATE DATA
-def run():
-    save_lists('cmv')
-    save_lists('td')
-    
-def _configure_logging():
-    os.makedirs(os.path.dirname(LOGGING_DIR), exist_ok=True)
-    file_handler = logging.handlers.TimedRotatingFileHandler(LOGGING_DIR, 
-                                                             when='W0', 
-                                                             backupCount=7)
-    
-    logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[logging.StreamHandler(),
-                              file_handler])
-
-run()    
-_configure_logging()
--- scraping_mod_sub_lists.py
+++ scraping_mod_sub_lists.py
@@ -23,7 +23,7 @@ names = df['name'].unique()[:5]
 
 ### CREATE EDGELIST + NODELIST FUNCTIONS
 
-def new_layout(name):
+def mod_list(name):
     #url = 'https://www.reddit.com/user/{}/moderated_subreddits.json'.format(name)
     #r = requests.get(url, {'user-agent':'ThisIsABot'})
     r = requests.get('https://www.reddit.com/user/{}/moderated_subreddits.json'.format(name), headers={'user-agent': 'ThisIsABot'})
@@ -39,34 +39,60 @@ def new_layout(name):
     if 'data' in data:
         for item in data['data']:
             d[item['sr']] = item['subscribers']
-    else:
+    elif 'message' in data:
         print(data['message'])
         d[data['message']] = data['message']
+    else:
+        d[0] = 0
     
     df = pd.DataFrame.from_dict(d, orient='index')
     df.reset_index(inplace=True)
     df.rename(columns={0 : 'subscribers', 'index':'sub'}, inplace=True)
     df['mod'] = name
+    df['date'] = date.today()
     
     time.sleep(2)
       
     return df
  
-sub = 'cmv'
-def run(sub):
+def edgelist(sub):
     df = pd.read_csv('/Users/emg/Programming/GitHub/mod-timelines/tidy-data/{}-mod-hist.csv'.format(sub), index_col=0)
     names = df['name'].unique()
     
     dfs = []
+    n = len(names)
     for name in names:
-        n = len(names)
         print(n, name)
-        dfs.append(new_layout(name))
+        dfs.append(mod_list(name))
         n -= 1
     
-    df = pd.concat(dfs)
-    df.reset_index(drop=True, inplace=True)
+    edgelist = pd.concat(dfs)
+    edgelist.reset_index(drop=True, inplace=True)
     
-    return df
+    edgelist_path = os.path.join('moding-data', '{}'.format(sub), str(date.today()), 'lists', 'edgelist')
+    os.makedirs(os.path.dirname(edgelist_path), exist_ok=True)
+    edgelist.to_csv(edgelist_path, index=False)
 
-df = run('cmv')
+    
+    return edgelist
+
+
+def nodelist(sub, edgelist):
+    df = pd.read_csv('/Users/emg/Programming/GitHub/mod-timelines/tidy-data/{}-mod-hist.csv'.format(sub), index_col=0)
+    nodelist = pd.DataFrame(list(set(edgelist['mod'])) + list(set(edgelist['sub'])))
+    modes = [1]*len(list(set(edgelist['mod']))) + [0]*len(list(set(edgelist['sub'])))
+    nodelist['type'] = modes
+    nodelist.rename(columns={0 : 'mod'}, inplace=True)
+    nodelist['mod_types'] = nodelist.apply(lambda row: get_mod_type(df, row['mod'], row['type']), axis=1)
+    
+    nodelist_path = os.path.join('moding-data', '{}'.format(sub), str(date.today()), 'lists', 'nodelist')
+    os.makedirs(os.path.dirname(nodelist_path), exist_ok=True)
+    nodelist.to_csv(nodelist_path, index=False)
+
+    return nodelist
+
+sub = 'cmv'
+edgelist = (sub)
+nodelist = nodelist(sub, edgelist)
+    
+
